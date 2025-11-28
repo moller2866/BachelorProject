af14ed830b2fd3f3c53c2882f9f4c783c4e5e52b
Moller2866 <kaspermoeller.t@gmail.com>
2025-11-28 19:19:12 +0100

    added trace to logs and metrics in provisioning

--- LTMO_Terraform/modules/grafana-provisioning/main.tf
+++ LTMO_Terraform/modules/grafana-provisioning/main.tf
@@ -39,7 +39,6 @@ resource "grafana_data_source" "tempo" {
     "X-Scope-OrgID" = "tenant1"
   }
 
-  # mTLS configuration (only applied if enabled)
   secure_json_data_encoded = var.enable_mtls ? jsonencode({
     tlsClientCert = var.grafana_client_cert
     tlsClientKey  = var.grafana_client_key
@@ -50,7 +49,34 @@ resource "grafana_data_source" "tempo" {
     tlsAuth           = var.enable_mtls
     tlsAuthWithCACert = var.enable_mtls
     tlsSkipVerify     = var.tls_skip_verify
+
+    # ---------------------------
+    # Trace to Logs configuration
+    # ---------------------------
+    tracesToLogs = {
+      datasourceUid      = grafana_data_source.loki.uid
+      spanStartTimeShift = "0s"
+      spanEndTimeShift   = "0s"
+      tags               = []
+      filterByTraceID    = true
+      filterBySpanID     = false
+      useCustomQuery     = false
+    }
+
+    # ------------------------------
+    # Trace to Metrics configuration
+    # ------------------------------
+    tracesToMetrics = {
+      datasourceUid      = grafana_data_source.mimir.uid
+      spanStartTimeShift = "-2m"
+      spanEndTimeShift   = "2m"
+      tags               = []
+    }
   })
+  depends_on = [
+    grafana_data_source.loki,
+    grafana_data_source.mimir
+  ]
 }
 
 resource "grafana_data_source" "mimir" {
